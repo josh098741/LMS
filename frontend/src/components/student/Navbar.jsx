0da96cda0f010355eaf37410c639efa2ec357f7e
josh098741 <morongemokaya@gmail.com>
2025-10-01 19:43:06 +0300

    Additional commits to nnav bar with addition of authentication with clerk

--- frontend/src/components/student/Navbar.jsx
+++ frontend/src/components/student/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {useClerk, UserButton, useUser} from '@clerk/clerk-react'
 
 import { assets } from '../../assets/assets';
 
@@ -7,19 +8,43 @@ function Navbar(){
 
     const isCourseListPage = location.pathname.includes('/course-list');
 
+    const {openSignIn} = useClerk()
+    const {user} = useUser()
 
     return(
         <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white':'bg-cyan-100/70'}`}>
             <img src={assets.logo} alt="IQL LOGO" className="w-28 lg:32 cursor-pointer"/>
             <div class="hidden md:flex items-center gap-5 text-gray-500">
                 <div className='flex items-center gap-5'>
-                    <button>Become Educator</button>
-                    | <Link to="/my-enrollments">My Enrollments</Link>
+                    {
+                        user && 
+                        <>
+                            <button>Become Educator</button>
+                            | <Link to="/my-enrollments">My Enrollments</Link>
+                        </>
+                    }
                 </div>
-                <button className="bg-blue-600 text-white px-5 py-2 rounded-full">Create Account</button>
+                {
+                    user ? <UserButton /> : <button onClick={() => openSignIn()} className="bg-blue-600 text-white px-5 py-2 rounded-full">Create Account</button>
+                }
             </div>
+
             {/*For smaller screens*/}
-            <div className="md:hidden"></div>
+            <div className="md:hidden flex items-center gap-2 sm:gap:5 text-gray-500">
+                <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
+                    {
+                        user && 
+                        <>
+                            <button>Become Educator</button>
+                            | <Link to="/my-enrollments">My Enrollments</Link>
+                        </>
+                    }
+                </div>
+                {
+                    user ? <UserButton /> : <button onClick={() => openSignIn()}><img src={assets.user_icon} /></button>
+                }
+            </div>
+
         </div>
     );
 }
