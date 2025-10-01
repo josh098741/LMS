9143511884cf359b7fd61c4a02c854fb31e83621
josh098741 <morongemokaya@gmail.com>
2025-10-01 18:20:46 +0300

    Additional commits

--- frontend/src/pages/educator/Educator.jsx
+++ frontend/src/pages/educator/Educator.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Outlet} from 'react-router-dom' 
 
 function Educator(){
 
     return(
         <div>
             <h1>Educator Page</h1>
+            <div>
+                {<Outlet/>}
+            </div>
         </div>
     );
 }
