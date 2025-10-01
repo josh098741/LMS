cb970e8289995dc9eb3b21191f1032ab222f50be
josh098741 <morongemokaya@gmail.com>
2025-10-01 21:27:48 +0300

    Additional commits

--- frontend/src/components/student/Hero.jsx
+++ frontend/src/components/student/Hero.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
 
+
 function Hero(){
+
     return(
         <div className="flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70">
             <h1 className="md:text-home-heading-large text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto">Empower your future with courses designed to 
@@ -17,6 +19,7 @@ function Hero(){
             <p className="md:hidden text-gray-500 max-w-sm mx-auto">
                 We bring together world class instructors to help you achieve your profesional goals
             </p>
+            <SearchBar />
         </div>
     );
 }
