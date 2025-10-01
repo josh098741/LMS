7d5a8e6f87124185e3e473cad3c911d6fe63374e
josh098741 <morongemokaya@gmail.com>
2025-10-01 22:30:37 +0300

    course section

--- frontend/src/components/student/CoursesSection.jsx
+++ frontend/src/components/student/CoursesSection.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function CoursesSection(){
 
     return(
-        <div>
-            <h1>Courses Section</h1>
+        <div className='py-16 md:px-40 px-8'>
+            <h2 className="text-3xl font-medium text-gray-800">Learn From The Best</h2>
+            <p className="text-sm md:text-base text-gray-500 mt-3"> 
+                Discover our top-rated courses across various categories.
+                From codding and design to business and 
+                wellness our courses are crafted to deliver results.
+            </p>
+
+            <Link to={'/course-list'} onClick={() => scrollTo(0,0)} 
+            className="text-gray-500 border border-gray-500/30 px-10 py-3 rounded">
+                Show All courses
+            </Link>
         </div>
     );
 }
