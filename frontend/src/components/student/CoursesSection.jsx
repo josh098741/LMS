051fcec456c4dc3fa98540e2f1b3714e8f6689ad
josh098741 <morongemokaya@gmail.com>
2025-10-02 17:18:30 +0300

    Fixed issue of sizing on the screen

--- frontend/src/components/student/CoursesSection.jsx
+++ frontend/src/components/student/CoursesSection.jsx
@@ -16,7 +16,7 @@ function CoursesSection(){
                 wellness our courses are crafted to deliver results.
             </p>
 
-            <div className="grid grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4">
+            <div className="grid grid-cols-auto px-4 md:px-0 md:my-16 my-10 gap-4">
                 {allCourses.slice(0,4).map((course,index) => <CourseCard key={index} course={course}/>)}
             </div>
 
