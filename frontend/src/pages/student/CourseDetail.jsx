3f6969d0b3646fc6a244af10e9ed71975b3d798d
josh098741 <morongemokaya@gmail.com>
2025-10-03 15:49:06 +0300

    Additional commit to course course detail

--- frontend/src/pages/student/CourseDetail.jsx
+++ frontend/src/pages/student/CourseDetail.jsx
@@ -36,7 +36,7 @@ function CourseDetail(){
             <div className="absolute top-0 left-0 w-full h-sectionHeight -z-1 bg-gradient-to-b from-cyan-100/70"></div>
             <div className="max-w-xl z-10 text-gray-500">
                 <h1 className="md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800">{courseData.courseTitle}</h1>
-                <p className="pt-4 mdd:text-base text-sm" dangerouslySetInnerHTML={{__html:courseData.courseDescription.slice(0,200)}}></p>
+                <p className="pt-4 md:text-base text-sm" dangerouslySetInnerHTML={{__html:courseData.courseDescription.slice(0,200)}}></p>
 
                 {/*review and rating*/}
                 <div className="flex items-center space-x-2 pt-3 pb-1 text-sm">
@@ -57,14 +57,14 @@ function CourseDetail(){
                     <div className="pt-5">
                         {courseData.courseContent.map((chapter,index) => (
                             <div key={index} className="border border-gray-300 bg-white mb-2 rounded">
-                                <div className="flex items-center justify-between px-4 py-3 cursor-pointer select-none">
+                                <div className="flex items-center justify-between px-4 py-3 cursor-pointer select-none" onClick={() => toggleSection(index)}>
                                     <div className="flex item-center gap-2">
-                                        <img src={assets.down_arrow_icon} alt="arrow icon" />
+                                        <img className={`transform transition-transform ${openSection[index] ? 'rotate-180':''}`} src={assets.down_arrow_icon} alt="arrow icon" />
                                         <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
                                     </div>
                                     <p className="text-sm md:text-default">{chapter.chapterContent.length} Lectures - {calculateChapterTime(chapter)}</p>
                                 </div>
-                                <div className="overflow-hidden transition-all duration-300 max-h-96">
+                                <div className={`overflow-hidden transition-all duration-300 ${openSection[index] ? 'max-h-96':'max-h-0'}`}>
                                     <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
                                         {chapter.chapterContent.map((lecture,index) => (
                                             <li key={index} className="flex items-start gap-2 py-1">
@@ -83,6 +83,10 @@ function CourseDetail(){
                             </div>
                         ))}
                     </div>
+                </div>
+                <div className="py-20 text-sm  md:text-default">
+                    <h3 className="text-xl font-semibold text-gray-800">Course Description</h3>
+                    <p className="pt-3 rich-text" dangerouslySetInnerHTML={{__html:courseData.courseDescription}}></p>
                 </div>
             </div>
 
