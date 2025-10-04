715826d757c3cda83aea4f47f9f7114e0d3ad050
josh098741 <morongemokaya@gmail.com>
2025-10-04 15:25:10 +0300

    Addition of sidebar to Educator

--- frontend/src/components/educator/Sidebar.jsx
+++ frontend/src/components/educator/Sidebar.jsx
@@ -1,9 +1,36 @@
-import React from  'react'
+import React,{useContext} from  'react'
+import {assets} from '../../assets/assets'
+import { AppContext } from '../../context/AppContext'
 
 function Sidebar(){
 
-    return(
-        <div>
+    const { isEducator } = useContext(AppContext)
+
+    const menuItems = [
+        {
+            name: 'Dashboard',
+            path: '/educator',
+            icon: assets.home_icon
+        },
+        {
+            name: 'Add Course',
+            path: '/educator/add-course',
+            icon: assets.add_icon
+        },
+        {
+            name: 'My Course',
+            path: '/educator/my-courses',
+            icon: assets.my_course_icon
+        },
+        {
+            name: 'Student Enrolled',
+            path: '/educator/student-enrolled',
+            icon: assets.person_tick_icon
+        }
+    ]
+
+    return isEducator && (
+        <div className="md:w-64 w-16 border-r min-h-screen text-base border-gray-500 py-2 flex flex-col">
             <h1>Sidebar</h1>
         </div>
     );
--- frontend/src/pages/educator/Educator.jsx
+++ frontend/src/pages/educator/Educator.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom' 
 import NavBar from '../../components/educator/Navbar';
+import Sidebar from '../../components/educator/Sidebar';
 
 function Educator(){
 
     return(
         <div className="text-default min-h-screen bg-white">
             <NavBar />
+            <Sidebar />
             <div>
                 {<Outlet/>}
             </div>
